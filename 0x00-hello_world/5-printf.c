0d9e6157de3c2bcee07ad3fee2f5efd38dcb99fc
Kawuribi <kawuribivincent@gmail.com>
2023-06-15 11:01:58 -0700

    6-size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Winning goal)
-*/
+ * main - a code that prints a line using print f function
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
-
