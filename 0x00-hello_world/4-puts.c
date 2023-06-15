c114a1987406e3c591b9e55da1832166d8e5fcc7
Kawuribi <kawuribivincent@gmail.com>
2023-06-15 06:31:05 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ * main - a program i intend to use to print with funtion
+ * Return: 0 (progress)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
